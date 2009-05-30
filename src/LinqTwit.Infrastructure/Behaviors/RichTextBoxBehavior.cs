e72e3d094f5f7aa8957bc228199845c5f8798aaa
Arild <arild.fines@broadpark.no>
2009-05-30 02:52:38 +0200

    Added behavior to databind RichTextBox.Document

---
+++ src/LinqTwit.Infrastructure/Behaviors/RichTextBoxBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace LinqTwit.Infrastructure.Behaviors
+{
+    public class RichTextBoxBehavior
+    {
+        /// <summary>
+        /// BindableDocument Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty BindableDocumentProperty =
+            DependencyProperty.RegisterAttached(
+                "BindableDocument",
+                typeof (FlowDocument),
+                typeof (RichTextBoxBehavior),
+                new PropertyMetadata(new PropertyChangedCallback(BindableDocumentChanged)));
+
+        public static void SetBindableDocument(RichTextBox obj,
+                                FlowDocument value)
+        {
+            obj.SetValue(BindableDocumentProperty, value);
+        }
+
+        public static FlowDocument GetBindableDocument(RichTextBox obj)
+        {
+            return (FlowDocument)obj.GetValue(BindableDocumentProperty);
+        }
+
+        private static void BindableDocumentChanged(DependencyObject d,
+                                     DependencyPropertyChangedEventArgs e)
+        {
+            RichTextBox box = d as RichTextBox;
+            FlowDocument doc = e.NewValue as FlowDocument;
+            if (box != null && doc != null)
+            {
+                box.Document = doc;
+            }
+        }
+    }
+}

8f2f1f222e929ba287494cb1e9ee38201daa22ee
Arild <arild.fines@broadpark.no>
2010-04-06 22:16:52 +0200

    Tests for update command (I think).

---
+++ src/LinqTwit.Commands.Tests/UI/ViewModels/UpdateCommandViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿using LinqTwit.TestUtilities;
+using NUnit.Framework;
+
+namespace LinqTwit.Commands.Tests.UI.ViewModels
+{
+    [TestFixture]
+    public class UpdateCommandViewModelTest : TestBase
+    {
+        protected override void OnSetup()
+        {
+            
+        }
+    }
+}
